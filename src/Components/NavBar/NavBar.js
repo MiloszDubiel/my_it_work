b63712edce0064e2c9dd1a80bd8329a7f3fdfda0
MiloszDubiel <syler2871@wp.pl>
2025-07-19 18:12:23 +0200

    Strona głowna

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,19 +1,39 @@
+import { useRef } from "react";
 import styles from "./navbar.module.css";
 import { CiSearch } from "react-icons/ci";
+import { VscAccount } from "react-icons/vsc";
 const Navbar = () => {
+  let searchBar = useRef(null);
   return (
     <nav className={styles.navBar}>
       <div className={styles.header}>
         <h3>MyITWork</h3>
         <div className={styles.searchBar}>
-          <label htmlFor="search">
-            <CiSearch />
+          <label
+            htmlFor="search"
+            onClick={() => {
+              searchBar.current.classList.toggle(`${styles.hiddenBar}`);
+            }}
+          >
+            <CiSearch className={styles.searchIcon} />
           </label>
-          <input type="search" id="search" />
+          <div
+            ref={searchBar}
+            className={styles.hiddenBar + " " + styles.barDiv}
+          >
+            <input
+              type="search"
+              id="search"
+              className={styles.searchBarInput}
+              placeholder="Szukaj ofert"
+            />
+          </div>
         </div>
       </div>
       <div className={styles.account}>
-        <button>Konto</button>
+        <button>
+          <VscAccount className={styles.icon} />
+        </button>
       </div>
     </nav>
   );
