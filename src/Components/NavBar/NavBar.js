b40f3c676ca9d389d61866eee9d68221716cffa5
MiloszDubiel <syler2871@wp.pl>
2025-07-21 14:39:23 +0200

    Ostateczne zmiany KONIEC zmian

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,41 +1,58 @@
-import { useRef } from "react";
+import { IoPersonOutline } from "react-icons/io5";
 import styles from "./navbar.module.css";
-import { CiSearch } from "react-icons/ci";
-import { VscAccount } from "react-icons/vsc";
+import { TbArrowNarrowDownDashed, TbArrowNarrowUpDashed } from "react-icons/tb";
+
 const Navbar = () => {
-  let searchBar = useRef(null);
   return (
-    <nav className={styles.navBar}>
-      <div className={styles.header}>
-        <h3>MyITWork</h3>
-        <div className={styles.searchBar}>
-          <label
-            htmlFor="search"
-            onClick={() => {
-              searchBar.current.classList.toggle(`${styles.hiddenBar}`);
-            }}
-          >
-            <CiSearch className={styles.searchIcon} />
-          </label>
-          <div
-            ref={searchBar}
-            className={styles.hiddenBar + " " + styles.barDiv}
-          >
-            <input
-              type="search"
-              id="search"
-              className={styles.searchBarInput}
-              placeholder="Szukaj ofert"
-            />
+    <header className={styles.headerElement}>
+      <nav className={styles.navBar}>
+        <div className={styles.header}>
+          <h3>MyITWork</h3>
+        </div>
+        <div className={styles.list}>
+          <ul>
+            <li>Oferty pracy</li>
+            <li>Pracodawcy IT</li>
+            <li>Kandydaci IT</li>
+          </ul>
+        </div>
+        <div className={styles.account}>
+          <div className={styles.addOffert}>
+            <p>Dodaj ogłoszenie</p>
+          </div>
+          <button>
+            <IoPersonOutline className={styles.icon} />
+          </button>
+        </div>
+      </nav>
+      <div className={styles.searchDiv}>
+        <h1>Rekrutacja IT, bez zbędnego kodu.</h1>
+        <div className={styles.formDiv}>
+          <div className={styles.localization}>
+            <p>Lokalizacja</p>
+            <p className={styles.arrow}>
+              <TbArrowNarrowDownDashed />
+            </p>
+          </div>
+          <div className={styles.position}>
+            <p>Stanoiwisko</p>
+            <p className={styles.arrow}>
+              <TbArrowNarrowDownDashed />
+            </p>
+
+          </div>
+          <div className={styles.technologies}>
+            <p>Technologia</p>
+            <p className={styles.arrow}>
+              <TbArrowNarrowDownDashed />
+            </p>
+          </div>
+          <div className={styles.filter}>
+            <p></p>
           </div>
         </div>
       </div>
-      <div className={styles.account}>
-        <button>
-          <VscAccount className={styles.icon} />
-        </button>
-      </div>
-    </nav>
+    </header>
   );
 };
 export default Navbar;
