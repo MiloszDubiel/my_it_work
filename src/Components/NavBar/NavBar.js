f7ea7c5e75dc4173127b9285017c3d578952dd08
MiloszDubiel <syler2871@wp.pl>
2025-07-22 13:21:54 +0200

    Nav kontynuacja

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,8 +2,22 @@ import { IoPersonOutline } from "react-icons/io5";
 import styles from "./navbar.module.css";
 import { IoFilterOutline } from "react-icons/io5";
 import { TbArrowNarrowDownDashed } from "react-icons/tb";
+import { CiSearch } from "react-icons/ci";
 
 const Navbar = () => {
+  const showMenu = (e) => {
+    if (e.currentTarget === e.target) {
+      e.target.lastChild.classList.toggle(`${styles.showMenu}`);
+      e.target.classList.toggle(`${styles.bordered}`);
+      e.target.querySelector("svg").classList.toggle(`${styles.rotate}`);
+    }
+    if (e.target.tagName === "P" || e.target.tagName === "svg") {
+      e.currentTarget.lastChild.classList.toggle(`${styles.showMenu}`);
+      e.currentTarget.classList.toggle(`${styles.bordered}`);
+      e.currentTarget.querySelector("svg").classList.toggle(`${styles.rotate}`);
+    }
+  };
+
   return (
     <header className={styles.headerElement}>
       <nav className={styles.navBar}>
@@ -29,23 +43,119 @@ const Navbar = () => {
       <div className={styles.searchDiv}>
         <h1>Rekrutacja IT, bez zbędnego kodu.</h1>
         <div className={styles.formDiv}>
-          <div className={styles.localization}>
+          <div
+            className={styles.localization}
+            onClick={(e) => {
+              showMenu(e);
+            }}
+          >
             <p>Lokalizacja</p>
             <p className={styles.arrow}>
               <TbArrowNarrowDownDashed />
             </p>
+            <div className={styles.localizations + " " + styles.showMenu}>
+              <div className={styles.searchBox}>
+                <CiSearch
+                  style={{
+                    position: "absolute",
+                    left: 0,
+                    transform: "translateX(23px)",
+                    color: "grey",
+                  }}
+                />
+                <input
+                  type="text"
+                  id="searchLocalization"
+                  placeholder="Szukaj..."
+                />
+              </div>
+              <div className={styles.options}>
+                <p>Remote</p>
+                <p>Rzeszów</p>
+                <p>Kraków</p>
+                <p>Warszawa</p>
+                <p>Poznań</p>
+                <p>Opole</p>
+                <p>Katowice</p>
+                <p>Wrocław</p>
+              </div>
+            </div>
           </div>
-          <div className={styles.position}>
-            <p>Stanoiwisko</p>
+          <div
+            className={styles.position}
+            onClick={(e) => {
+              showMenu(e);
+            }}
+          >
+            <p>Stanowisko</p>
             <p className={styles.arrow}>
               <TbArrowNarrowDownDashed />
             </p>
+            <div className={styles.positions + " " + styles.showMenu}>
+              <div className={styles.searchBox}>
+                <CiSearch
+                  style={{
+                    position: "absolute",
+                    left: 0,
+                    transform: "translateX(23px)",
+                    color: "grey",
+                  }}
+                />
+                <input
+                  type="text"
+                  id="searchLocalization"
+                  placeholder="Szukaj..."
+                />
+              </div>
+              <div className={styles.options}>
+                <p>Remote</p>
+                <p>Rzeszów</p>
+                <p>Kraków</p>
+                <p>Warszawa</p>
+                <p>Poznań</p>
+                <p>Opole</p>
+                <p>Katowice</p>
+                <p>Wrocław</p>
+              </div>
+            </div>
           </div>
-          <div className={styles.technologies}>
+          <div
+            className={styles.technologie}
+            onClick={(e) => {
+              showMenu(e);
+            }}
+          >
             <p>Technologia</p>
             <p className={styles.arrow}>
               <TbArrowNarrowDownDashed />
             </p>
+            <div className={styles.technologies + " " + styles.showMenu}>
+              <div className={styles.searchBox}>
+                <CiSearch
+                  style={{
+                    position: "absolute",
+                    left: 0,
+                    transform: "translateX(23px)",
+                    color: "grey",
+                  }}
+                />
+                <input
+                  type="text"
+                  id="searchLocalization"
+                  placeholder="Szukaj..."
+                />
+              </div>
+              <div className={styles.options}>
+                <p>Remote</p>
+                <p>Rzeszów</p>
+                <p>Kraków</p>
+                <p>Warszawa</p>
+                <p>Poznań</p>
+                <p>Opole</p>
+                <p>Katowice</p>
+                <p>Wrocław</p>
+              </div>
+            </div>
           </div>
           <div className={styles.filter}>
             <p>
